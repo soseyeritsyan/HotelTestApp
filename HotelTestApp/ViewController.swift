bed7d12540ee21451eac87c12fb04fa04c23f360
sose yeritsyan <sose.yeritsyan@gmail.com>
2023-12-19 15:22:14 +0400

    first commit

---
+++ HotelTestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HotelTestApp
+//
+//  Created by sose yeritsyan on 19.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slideShowCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
